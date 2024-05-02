d8938849520b731e1a8055295ae220a3276b017d
Nighty3098 <night3098games@gmail.com>
2024-05-02 15:48:47 +0700

    Edited

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         blocks.forEach(block => {
             let blockPosition = block.getBoundingClientRect().top;
 
-            if (blockPosition < windowHeight - 100) {
+            if (blockPosition < windowHeight - 200) {
                 block.style.opacity = "1";
                 block.style.transform = "translateY(0)";
             }
@@ -16,5 +16,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     window.addEventListener('scroll', checkBlocksVisibility);
 });
-
-
