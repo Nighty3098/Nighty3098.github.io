77e6d58971d5d8de02498695547e510abf0a5e44
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-08-19 22:28:18 +0700

    Added top lang list for GitHub card

--- js/git.js
+++ js/git.js
@@ -1,3 +1,6 @@
+
+const username = 'Nighty3098';
+
 async function fetchGitHubUserData(username) {
     const url = `https://api.github.com/users/${username}`;
     try {
@@ -33,13 +36,48 @@ async function fetchGitHubUserRepos(username) {
     }
 }
 
-function displayUserData(data) {
-    document.getElementById('git_followers').textContent = `Followers: ${data.followers}`;
-    document.getElementById('git_following').textContent = `Following: ${data.following}`;
-    document.getElementById('git_repos').textContent = `Repos: ${data.public_repos}`;
-    document.getElementById('git_avatar').src = data.avatar_url;
+async function getTopUserLanguages(username) {
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+        const repos = await response.json();  
+        const languageCounts = {};
+
+        repos.forEach(repo => {
+            const language = repo.language;
+    
+            if (language) {
+            languageCounts[language] = (languageCounts[language] || 0) + 1;
+            }
+        });
+    
+        const sortedLanguages = Object.entries(languageCounts)
+            .sort(([, a], [, b]) => b - a)
+            .map(([language]) => language);
+    
+    return sortedLanguages.slice(0, 5);
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 }
 
-const username = 'Nighty3098';
+function displayUserData(data) {
+    getTopUserLanguages(username).then(topLanguages => {
+        console.log(`Top langs ${username}:`, topLanguages);
+        const topLanguagesText = topLanguages.join(', ');
+        document.getElementById('top-languages').textContent = 'Top langs: ' + topLanguagesText;
+    });
+
+    document.getElementById('git_followers').textContent = `Followers: ${data.followers}`;
+    console.log(`Followers ${username}:`, data.followers);
+    document.getElementById('git_following').textContent = `Following: ${data.following}`;
+    console.log(`Following ${username}:`, data.following);
+    document.getElementById('git_repos').textContent = `Repos: ${data.public_repos}`;
+    console.log(`Repos ${username}:`, data.public_repos);
+    document.getElementById('git_avatar').src = data.avatar_url;
+    console.log(`git_avatar ${username}:`, data.avatar_url);
+}
+
 fetchGitHubUserData(username);
 fetchGitHubUserRepos(username);
+getTopUserLanguages(username);
