7123d2ecb3217bd931836332808956ff9c260d3e
Nighty3098 <night3098games@gmail.com>
2025-02-22 12:27:31 +0700

    Update bio.jsx

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -9,38 +9,29 @@ import GitHubStats from "./git_cards";
 const Bio = () => {
   const controls = useAnimation();
 
-  const textVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-  };
-
-  const imageVariants = {
-    hidden: { opacity: 0, scale: 0.8 },
-    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-  };
-
-  const buttonVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.2 } },
-  };
-
   return (
     <motion.div
       className="large_block"
-      initial="hidden"
-      whileInView="visible"
-      exit="exit"
       style={{ gap: "50px" }}
-      viewport={{ margin: "0px 0px -100px 0px", amount: 0.2 }}
     >
       <motion.div className="bio_content" id="bio">
         <motion.img
           src="/me.png"
           className="user_avatar"
-          variants={imageVariants}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={controls}
+          viewport={{ margin: "0px 0px -50px 0px", amount: 0.1 }}
+          onViewportEnter={() => {
+            controls.start({
+              opacity: 1,
+              scale: 1,
+              transition: {
+                duration: 0.5,
+                type: "spring",
+                stiffness: 100,
+              },
+            });
+          }}
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         />
@@ -59,32 +50,53 @@ const Bio = () => {
               style={{ marginTop: "-80px", marginLeft: "-30px" }}
             />
           </motion.div>
+
           {[
             "Hello. I am Artem, a backend developer and freelancer.",
-            "I am 18 years old, I am in the 11th grade of school and have been at the programming academy for 5 years.",
+            "I am 18 years old, I am in the 11th grade of school and have been at the programming academy for 4 years.",
             "I strive to develop in the field of development and want to work on large projects that can affect people's lives.",
             "You can view all the projects on my github.",
           ].map((text, index) => (
             <motion.div
               key={index}
               style={{ marginBottom: "30px" }}
-              variants={textVariants}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true }}
-              transition={{ delay: index * 0.2 }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={controls}
+              viewport={{ margin: "0px 0px -50px 0px", amount: 0.1 }}
+              onViewportEnter={() => {
+                controls.start({
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.5,
+                    delay: index * 0.1,
+                    type: "spring",
+                    stiffness: 100,
+                  },
+                });
+              }}
             >
               {text}
             </motion.div>
           ))}
-          <motion.div></motion.div>
         </motion.div>
         <motion.a
           href="https://github.com/Nighty3098"
-          variants={buttonVariants}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={controls}
+          viewport={{ margin: "0px 0px -50px 0px", amount: 0.1 }}
+          onViewportEnter={() => {
+            controls.start({
+              opacity: 1,
+              y: 0,
+              transition: {
+                duration: 0.5,
+                delay: 0.4,
+                type: "spring",
+                stiffness: 100,
+              },
+            });
+          }}
           whileHover={{ scale: 1.0, width: "350px" }}
           whileTap={{ scale: 0.95 }}
         >
@@ -97,6 +109,7 @@ const Bio = () => {
           </button>
         </motion.a>
       </motion.div>
+
       <Info />
       <GitHubStats username={"Nighty3098"} />
     </motion.div>
