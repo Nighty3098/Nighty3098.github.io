b8f869f62be77c5287222c8f9ca04255e730edd0
Nighty3098 <night3098games@gmail.com>
2025-02-26 19:56:06 +0700

    Update links.jsx

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 
 const Links = () => {
+  const controls = useAnimation();
+
   const containerStyle = {
     width: "100%",
     display: "flex",
@@ -16,21 +18,24 @@ const Links = () => {
 
   const animationProps = {
     initial: { opacity: 0, y: 20 },
-    whileInView: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.5,
-        type: "spring",
-        stiffness: 100,
-      },
-    },
+    animate: controls,
     viewport: { margin: "0px 0px -50px 0px", amount: 0.1 },
+    onViewportEnter: () => {
+      controls.start({
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 0.5,
+          type: "spring",
+          stiffness: 100,
+        },
+      });
+    },
   };
 
   return (
     <div style={containerStyle}>
-      <motion.a href="https://owl-gamma.vercel.app/" {...animationProps}>
+      <motion.a href="owl-gamma.vercel.app/" {...animationProps}>
         <button className="button" id="projects_btn">
           TRY MY PROJECT MANAGER <FontAwesomeIcon icon={faCodeBranch} />
         </button>
